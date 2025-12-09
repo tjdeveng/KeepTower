5a6fb304e948ba70b9b9fe555d7b76e8306e01e0
dev <dev@tjdeveng.com>
2025-12-09 21:19:03 +0000

    fix: update run.sh to use compiled schema from build directory
    The schema is now auto-compiled to build/data/gschemas.compiled during
    the build process, so run.sh should point to build/data instead of data/
    to use the compiled schema.
    
    This matches the fix from commit 8a44ef9 where we added automatic schema
    compilation to the build process.

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 # Convenience script to run KeepTower with local GSettings schema
 
 cd "$(dirname "$0")"
-GSETTINGS_SCHEMA_DIR=$PWD/data ./build/src/keeptower "$@"
+GSETTINGS_SCHEMA_DIR=$PWD/build/data ./build/src/keeptower "$@"
